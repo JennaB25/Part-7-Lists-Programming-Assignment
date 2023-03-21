db36c5e5edde4847170082dbbe13553a9b09fa17
362700239 <jennarbloor@gmail.com>
2023-03-21 09:55:41 -0400

    Add project files.

---
+++ Part 7 Lists Programming Assignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Part_7_Lists_Programming_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random generator = new Random();
+            List<int> randomNum = new List<int>() { 24 };           
+            for (int i = 0; i < randomNum.Count; i++)
+            {
+                randomNum[i] = generator.Next(10, 21);               
+                Console.WriteLine(randomNum[i]);
+            }
+                
+
+        }   
+    }
+}
